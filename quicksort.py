6ab229e139e9a26fb35096e73d3f275712a93e01
Emerson Victor <evfl@cin.ufpe.br>
2017-10-25 15:56:11 -0300

    :rocket: quicksort1 and patition implemented

---
+++ quicksort.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+'''
+Universidade Federal de Pernambuco (UFPE) (http://www.ufpe.br)
+Centro de Informática (CIn) (http://www.cin.ufpe.br)
+Graduando em Sistemas de Informação
+IF969 - Algoritmos e estrutura de dados
+
+Autor:	Emerson Victor Ferreira da Luz (evfl)
+Email:	evfl@cin.ufpe.br
+Data:	2017-10-22
+
+Copyright(c) 2017 Emerson Victor
+'''
+
+def quickSort1(array):
+    #Ordenação quicksort com implementação utilizando método quebra e concatenação de lista
+    if array == []:
+        return []
+    else:
+        pivot = array.pop(0)
+        return quickSort1([num for num in array if num < pivot ]) + [pivot] + quickSort1([num for num in array if num >= pivot])
+
+def quickSort2(array):
+    #Ordenação quicksort com implementação utilizando método de partição
+    pass
+
+
+def partition(array, left, right):
+    #Divisão do array em duas partes, menores que o pivô e maiores que o pivô
+    pivot = array[left]
+    cont = True
+    l = left + 1
+    r = right
+
+    while cont:
+        while array[l] < pivot:
+            if l >= right:
+                break
+            l += 1
+
+        while array[r] > pivot:
+            if r <= left:
+                break
+            r -= 1
+
+        if l >= r:
+            cont = False
+        else:
+            array[l], array[r] = array[r], array[l]
+
+    array[left], array[r] = array[r], array[left]
+    return r
