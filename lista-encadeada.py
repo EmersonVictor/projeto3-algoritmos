9d8ce9ae16ad28cdc02fd6568e41ef7187b6e969
Emerson Victor <evfl@cin.ufpe.br>
2017-10-27 11:14:27 -0300

    :rocket: List implemented

--- lista-encadeada.py
+++ lista-encadeada.py
@@ -18,6 +18,9 @@ class Item:
         self.__key = key
         self.__value = value
 
+	def __repr__(self):
+		return "Key:{0} - Value:{1}".format(self.__key,self.__value)
+
     def getKey(self):
         return self.__key
 
@@ -39,14 +42,8 @@ class Node:
 
 class Dictionary:
     #Implementação de um dicionário por meio de lista encadeada
-    def __init__(self,raiz=None,first=None,last=None):
-        self.__raiz = Node() = self.__first = self.__last
-
-    def setFirst(self,node):
-        self.__first = node
-
-    def setLast(self,node):
-        self.__last = node
+    def __init__(self,first=None,last=None):
+        self.__first = self.__last = Node()
 
     def empty(self):
         #Verifica se o dicionário está vazio e devolve um valor bool referente ao resultado
@@ -69,16 +66,18 @@ class Dictionary:
         self.__last = Node(self.__last,item,None)
         self.__last = self.__last.next
 
-    def removeEnd(self):
-        #Remove elemento do fim
-        if self.empty():
-            raise IndexError ("Remove from empty list")
+	def insertOrdered(self,item):
+		#Insere um item de forma ordenada
+		aux = self.__first.next
+		while not(aux is None) and not(aux.item.getKey() > key):
+			aux = aux.next
 
-        aux = self.__last
-        aux.before.next = aux.next
-        self.__last = aux.before
-        aux.before = None
-        del aux
+		if aux is None:
+			self.__last.next = Node(self.__last, item, None)
+			self.__last = self.__last.next
+		else:
+			aux.before.next = Node(aux.before,item,aux)
+			aux.before = aux.before.next
 
     def removeStart(self):
         #Remove elemento do início
@@ -95,3 +94,67 @@ class Dictionary:
 
         aux.next = aux.before = None
         del aux
+
+    def removeEnd(self):
+        #Remove elemento do fim
+        if self.empty():
+            raise IndexError ("Remove from empty list")
+
+        aux = self.__last
+        aux.before.next = aux.next
+        self.__last = aux.before
+        aux.before = None
+        del aux
+
+	def removeKey(self, key):
+		#Remove elemento de acordo com uma chave
+		if self.empty():
+			raise IndexError ("Remove from empty list")
+
+		aux = self.__first.next
+		while not(aux is None) and not(aux.item.getKey() == key):
+			aux = aux.next
+
+		if aux is None:
+			raise KeyError("Key not found")
+		else:
+			aux.before.next = aux.next
+			if not (aux.next is None):
+				aux.next.before = aux.before
+			if self.__last is aux:
+				self.__last = aux.before
+			aux.next = aux.before = None
+			del aux
+
+	def searchKey(self, key):
+		#Pesquisa elemento de acorco com uma chave
+		if self.empty():
+			raise IndexError ("Search on empty list")
+
+		aux = self.__first.next
+		while not(aux is None) and not(aux.item.getKey() == key):
+			aux = aux.next
+
+		if aux is None:
+			raise KeyError("Key not found")
+		else:
+			return aux.item
+
+	def printItems(self,start=1):
+		#Imprime todos os elementos da lista na tela
+
+		if self.empty() == True:
+			raise IndexError("Print from empty list")
+
+		aux = self.__first.next
+		startAt = 1
+
+		while startAt < start:
+			if aux is None:
+				raise IndexError("Index out of range")
+			aux = aux.next
+			startAt += 1
+
+		while not(aux is None):
+			print(aux)
+			aux = aux.next
