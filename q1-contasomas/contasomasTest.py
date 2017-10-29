b7924f94af9133ebede7f6d51b860d048a67d0e5
Emerson Victor <evfl@cin.ufpe.br>
2017-10-29 17:09:00 -0300

    :bug: Bugs fixed

--- q1-contasomas/contasomasTest.py
+++ q1-contasomas/contasomasTest.py
@@ -12,10 +12,9 @@ Data:	2017-10-22
 Copyright(c) 2017 Emerson Victor
 '''
 from time import perf_counter
-from conta_somas import *
 import sys
+from conta_somas import *
 import numpy
-
 
 MAX = 999999
 
@@ -32,22 +31,22 @@ def gera_seq_aleatoria(tam):
    return numpy.random.randint(-MAX,MAX, size=tam)
 
 def main():
-    resultados = open("cs-results.txt, a+")
-	seeds = [11,7,13,19,5189,7919]
-	tam = [50,100,250,500,1000]
-	for i,seed in enumerate(seeds):
-	   numpy.random.seed(seed)
-	   vetor = gera_seq_aleatoria(tam[i])
-	   cron = Cronometro()
-	   total = countSums(vetor)
+    resultados = open("cs-results.txt", "a+")
+    seeds = [11,7,13,19,5189,7919]
+    tam = [50,100,250,500,1000,1500]
+    for i,seed in enumerate(seeds):
+       numpy.random.seed(seed)
+       vetor = gera_seq_aleatoria(tam[i])
+       cron = Cronometro()
+       total = countSums(vetor)
        resultado = "Tempo gasto com {0} elementos foi {1} segundos".format(tam[i],cron.tempo_gasto())
-	   print(resultado)
+       print(resultado)
        resultados.write(resultado + "\n")
-	   del vetor
-	   del cron
+       del vetor
+       del cron
 
     resultados.write("\n")
     resultados.close()
 
 if __name__ == '__main__':
-	main()
+    main()
